16cf8662ddefa06840f26b5f22c23b8de4fdfb82
Zeta <158331793+ZetaPossibly@users.noreply.github.com>
2026-04-06 12:46:51 +0100

    Update fix_chat.js

--- fix_chat.js
+++ fix_chat.js
@@ -1,23 +1,25 @@
 // ==UserScript==
 // @name         GeoFS Chat Fix
 // @namespace
-// @version      0.0.2
-// @description  Fixes the removal of the t keybind for opening the chat window in GeoFS.
+// @version      0.0.3
+// @description  Fixes the removal of the t keybind for opening the chat window in GeoFS. Now with lower latency!
 // @author       Zeta
-// @license      
-// @match        https://www.geo-fs.com/geofs.php*
+// @license
+// @updateURL    https://raw.githubusercontent.com/ZetaPossibly/GeoFS-Chat-Fix/refs/heads/main/fix_chat.js
+// @downloadURL  https://raw.githubusercontent.com/ZetaPossibly/GeoFS-Chat-Fix/refs/heads/main/fix_chat.js
+// @match        https://*.geo-fs.com/geofs.php*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
 // ==/UserScript==
 
 
 (function() {
-    window.addEventListener("keyup", function(e) {
+    window.addEventListener("keydown", function(e) {
         // Only trigger when no input field is focused
         if (document.activeElement.tagName.toLowerCase() !== "input" &&
             document.activeElement.tagName.toLowerCase() !== "textarea") {
-            
-            // T key (keyCode 84)
-            if (e.keyCode === 84) {
+
+            if (e.key === "t" || e.key === "T") {
+                e.preventDefault();
                 e.stopPropagation();
                 ui.chat.showInput();
             }
